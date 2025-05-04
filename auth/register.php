ea07ce3e2867aa9872540530f0be94a68d916979
binggo_qeihannn <binggoqeihan@gmail.com>
2025-05-04 15:40:37 +0700

    perbaikan fitur register

--- auth/register.php
+++ auth/register.php
@@ -1,8 +1,10 @@
-<?php include(".layouts/header.php"); ?>
-<!-- Register Card -->
+<?php include(".layouts/header.php"); ?> <!-- Menyertakan file header -->
+
+<!-- Kartu Registrasi -->
 <div class="card">
   <div class="card-body">
-    <!-- Logo -->
+
+    <!-- Logo Aplikasi -->
     <div class="app-brand justify-content-center">
       <a href="index.html" class="app-brand-link gap-2">
         <span class="app-brand-logo demo"></span>
@@ -10,22 +12,42 @@
       </a>
     </div>
     <!-- /Logo -->
+
+    <!-- Form Registrasi -->
     <form action="register_process.php" class="mb-3" method="POST">
+      
+      <!-- Input Nama -->
       <div class="mb-3">
         <label for="namaTamu" class="form-label">NAMA</label>
-        <input type="text" class="form-control" name="namaTamu" placeholder="Masukkan nama Tamu" autofocus/>
+        <input type="text" class="form-control" name="namaTamu" placeholder="Masukkan namaTamu" autofocus/>
       </div>
+
+      <!-- Input Email -->
       <div class="mb-3">
         <label for="Email" class="form-label">EMAIL</label>
         <input type="text" class="form-control" name="Email" placeholder="Masukkan Email" />
-
       </div>
+
+      <!-- Pilihan Role -->
+      <div class="mb-3">
+        <label class="form-label" for="role">LOGIN SEBAGAI</label>
+        <select name="role" class="form-control" required>
+          <option value="user">Tamu</option>
+          <option value="admin">Admin</option>
+        </select>
+      </div>
+
+      <!-- Tombol Submit -->
       <button class="btn btn-primary d-grid w-100">Daftar</button>
     </form>
+
+    <!-- Link ke halaman login jika sudah memiliki akun -->
     <p class="text-center">
       <span>Sudah memiliki akun?</span><a href="login.php"><span> Masuk</span></a>
     </p>
+
   </div>
 </div>
-<!-- Register Card -->
+
+
 <?php include(".layouts/footer.php"); ?>

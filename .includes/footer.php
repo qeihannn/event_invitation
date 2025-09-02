e2dfb1847574178a04ca8c5fe9128b09821cbfa8
binggo_qeihannn <binggoqeihan@gmail.com>
2025-09-02 09:24:30 +0700

    footer

--- .includes/footer.php
+++ .includes/footer.php
@@ -1,4 +1,3 @@
-<!-- Footer -->
 <footer class="content-footer footer bg-footer-theme">
               <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
                 <div class="mb-2 mb-md-0">
@@ -14,35 +13,25 @@
                 </div>
               </div>
             </footer>
-            <!-- / Footer -->
             <div class="content-backdrop fade"></div>
           </div>
-          <!-- Content wrapper -->
         </div>
-        <!-- / Layout page -->
       </div>
-      <!-- Overlay -->
       <div class="layout-overlay layout-menu-toggle"></div>
     </div>
-    <!-- / Layout wrapper -->
-
-    <!-- Core JS -->
-    <!-- build:js assets/vendor/js/core.js -->
     <script src="assets/vendor/libs/jquery/jquery.js"></script>
     <script src="assets/vendor/js/bootstrap.bundle.min.js"></script>
     <script src="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
     <script src="assets/vendor/js/menu.js"></script>
-    <!-- endbuild -->
-    <!-- Main JS -->
     <script src="assets/js/main.js"></script>
-    <!-- Datatables JS -->
+
     <script src="assets/vendor/libs/datatables/jquery.dataTables.min.js"></script>
     <script src="assets/vendor/libs/datatables/dataTables.bootstrap5.min.js"></script>
     <script src="assets/vendor/libs/datatables/datatables.js"></script>
-    <!-- Summernote JS -->
+
     <script src="assets/vendor/libs/summernote/summernote.min.js" defer></script>
     <script src="assets/vendor/libs/summernote/summernote.js" defer></script>
-    <!-- Active State JS -->
+
     <script src="assets/js/active-state-nav.js"></script>
 </body>
 
